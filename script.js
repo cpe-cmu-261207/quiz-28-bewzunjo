da942febce02751081eecf9adca0175bef26de74
bewzunjo <90750971+bewzunjo@users.noreply.github.com>
2022-08-04 14:34:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const genderTag = document.getElementById("span-gender-icon");
+const imgTag = document.getElementById("img-profile");
+const nameTag = document.getElementById("p-name");
+const addressTag = document.getElementById("p-address");
+const emailTag = document.getElementById("p-email");
+const btnRandom = document.getElementById("btn-random");
+const maincard = document.getElementById("div-user-card");
+const loadcard = document.getElementById("div-loading-card");
+
+btnRandom.onclick = () => {
+  callApi();
+  maincard.style.display = "";
+  loadcard.style.display = "none";
+};
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  if (resp.data.results[0].gender == "female") {
+    genderTag.innerText = "👩";
+  } else {
+    genderTag.innerText = "👨";
+  }
+  imgTag.src = resp.data.results[0].picture.large;
+  nameTag.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  emailTag.innerText = resp.data.results[0].email;
+  addressTag.innerText =
+    resp.data.results[0].location.city +
+    " " +
+    resp.data.results[0].location.state +
+    " " +
+    resp.data.results[0].location.country +
+    " " +
+    resp.data.results[0].location.postcode;
+  console.log(resp.data.results[0]);
 }
+
+btnRandom.onload = () => {
+  maincard.style.display = "none";
+  loadcard.style.display = "";
+};
